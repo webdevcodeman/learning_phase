47eff99c13e1303df74c84d8ab1f4437ad32c860
webdev <117325718+webdevcodeman@users.noreply.github.com>
2023-01-15 19:30:24 +0530

    Understand JavaScript if, else if, else

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -27,7 +27,37 @@
 // let sum=1+2+3+4+5+6;
 // console.log(sum);
 // console.log(((4+6+9)/77).toFixed(5));
-const max=57;
-let actual=max-13;
-let percentage=actual/max;
-percentage=percentage.toFixed(4);
+// const max=57;
+// let actual=max-13;
+// let percentage=actual/max;
+// percentage=percentage.toFixed(4);
+// let userName=prompt("Who's there?","");
+// if(userName=== "" || userName === null){
+//   alert("Canceled");
+// }else if(userName !== "Admin"){
+//   alert("I don't know you");
+// }else{
+//   let password=prompt("Password?","");
+//   if(password=== "" || password===null){
+//     alert("Canceled");
+//   }else if(password!== "TheMaster"){
+//     alert("Wrong password");
+//   }else{
+//     alert("Welcome!");
+//   }
+// }
+let userName=prompt("Who's there?","");
+if(userName === "Admin"){
+  let password=prompt("Password?","");
+  if(password === "TheMaster"){
+    alert("Welcome!");
+  }else if(password=== "" || password===null){
+    alert("Canceled");
+  }else{
+    alert("Wrong password");
+  }
+}else if(userName=== "" || userName === null){
+  alert("Canceled");
+}else{
+  alert("I don't know you");
+}
