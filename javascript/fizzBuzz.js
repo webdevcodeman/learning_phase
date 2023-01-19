3bc99a31b04386294cc89497ae83b47a6aa0d947
webdev <117325718+webdevcodeman@users.noreply.github.com>
2023-01-19 13:00:42 +0530

    Print "Fizz" x%3==0, "Buzz" x%5==0 "FizzBuzz" both

---
+++ javascript/fizzBuzz.js
@@ -0,0 +1,13 @@
+"use strict";
+let num=parseInt(prompt("Please enter a number till you want FizzBuzz to: "));
+for(let i=1;i<=num;i++){
+  if(i%5===0 && i%3===0){
+    console.log("FizzBuzz");
+  }else if(i%3===0){
+    console.log("Fizz");
+  }else if(i%5===0){
+    console.log("Buzz");
+  }else{
+    console.log(i);
+  }
+}
